e8120bfb67c1983af295ea3319b8ffb0089d68c8
Dan Stronge <dnlstronge@gmail.com>
2023-02-26 09:03:12 +0000

    [x] - create button [x] - change button to UL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from "@mui/material/Button"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <span>Material UI example website</span>
+
+        <div>
+          <Button variant="outlined">Click Me</Button>
+        </div>
       </header>
     </div>
   );
